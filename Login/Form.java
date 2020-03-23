2e06d0b3af70b62ca541ce08b1b4262a1788fe5f
CaHerm <cam2000h@gmail.com>
2020-03-22 21:50:21 -0600

    Updated Form
    Form now allows the user to choose if they are registering as a student or scholarship coordinator. It will write to either "students.txt" or "coordinators.txt" depending on their choice.
    
    Form now accepts the user's date of birth through drop down menus with set values.

--- Login/Form.java
+++ Login/Form.java
@@ -44,6 +44,8 @@ import java.awt.event.KeyListener;
 import java.awt.event.InputMethodEvent;
 import java.awt.event.KeyAdapter;
 import javax.swing.SwingConstants;
+import javax.swing.JList;
+import javax.swing.JComboBox;
 /**
  * @author Soumya Kumaria
  * This class takes input from the user to create a new user and register such that
@@ -54,14 +56,11 @@ public class Form {
 	private JFrame frame;
 	private JTextField firstName;
 	private JTextField lastName;
-	private JTextField birthYear;
 	private static final String[] args = null;
 	private JTextField emailID;
 	private JTextField studentID;
 	private JPasswordField passwordField;
 	private String s;
-	private JTextField birthMonth;
-	private JTextField birthDay;
 	private JTextField schoolName;
 	
 	/**
@@ -118,11 +117,6 @@ public class Form {
 		lastName = new JTextField();
 		lastName.setColumns(10);
 		
-		JLabel lblDateOfBirth = new JLabel("Birth year:");
-		
-		birthYear = new JTextField();
-		birthYear.setColumns(10);
-		
 		JLabel lblEmailAddress = new JLabel("email address:");
 		
 		emailID = new JTextField();
@@ -140,107 +134,116 @@ public class Form {
 		JLabel lblLastName = new JLabel("Last Name:");
 		lblLastName.setHorizontalAlignment(SwingConstants.RIGHT);
 		
-		JLabel lblBirthMonth = new JLabel("Birth month:");
-		
-		birthMonth = new JTextField();
-		birthMonth.setColumns(10);
-		
-		JLabel lblBirthDay = new JLabel("Birth day:");
-		
-		birthDay = new JTextField();
-		birthDay.setColumns(10);
-		
 		JLabel lblSchoolsName = new JLabel("School's name:");
 		
 		schoolName = new JTextField();
 		schoolName.setColumns(10);
 		
+		String[] roles = {"student", "scholarship coordinator"};
+		JComboBox role = new JComboBox(roles);
+		
+		JLabel lblAccountType = new JLabel("Account Type:");
+		
+		JComboBox month = new JComboBox(new String[] {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"});
+		
+		String[] days = new String[31];
+		for(int i = 1; i <= 31; i ++) {
+			days[i - 1] = Integer.toString(i);
+		}
+		JComboBox day = new JComboBox(days);
+		
+		String[] years = new String[120];
+		for(int i = 2020; i > 1900; i--) {
+			years[2020 - i] = Integer.toString(i);
+		}
+		JComboBox year = new JComboBox(years);
+		
+		JLabel lblDateOfBirthmdy = new JLabel("Date of Birth (M/D/Y):");
+		
 		JButton btnSubmit = new JButton("Submit");
 		/**
-		 * This action listener is used to store the contents of the form window into a file named
-		 * "users.txt". The contents are stored onto the file after the submit button is clicked. 
+		 * This action listener is used to store the contents of the form window into either
+		 * "students.txt" or "coordinators.txt". The contents are stored onto the file after 
+		 * all boxes are filled and the submit button is clicked. 
 		 */
 		btnSubmit.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
-				String filename = "users.txt";
-				try {
-					FileWriter writer = new FileWriter(filename,true);
-					firstName.write(writer);
-					writer.append("\n");
-					lastName.write(writer);
-					writer.append("\n");
-					birthYear.write(writer);
-					writer.append("\n");
-					birthMonth.write(writer);
-					writer.append("\n");
-					birthDay.write(writer);
-					writer.append("\n");
-					schoolName.write(writer);
-					writer.append("\n");
-					emailID.write(writer);
-					writer.append("\n");
-					studentID.write(writer);
-					writer.append("\n");
-					passwordField.write(writer);
-					writer.append("\n");
-					 writer.close();
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
+				if(!firstName.getText().equals("") && !lastName.getText().equals("") && !year.getSelectedItem().equals("") && !month.getSelectedItem().equals("")
+						&& !day.getSelectedItem().equals("") && !schoolName.getText().equals("") && !emailID.getText().equals("") && !studentID.getText().equals("")
+						&& !passwordField.getText().equals("")) {
+					
+					String filename = role.getSelectedItem().equals("student") ? "students.txt" : "coordinators.txt";
+					try {
+						FileWriter writer = new FileWriter(filename,true);
+						firstName.write(writer);
+						writer.append("\n");
+						lastName.write(writer);
+						writer.append("\n");
+						writer.append(year.getSelectedItem() + "\n");
+						writer.append(month.getSelectedItem() + "\n");
+						writer.append(day.getSelectedItem() + "\n");
+						schoolName.write(writer);
+						writer.append("\n");
+						emailID.write(writer);
+						writer.append("\n");
+						studentID.write(writer);
+						writer.append("\n");
+						passwordField.write(writer);
+						writer.append("\n ");
+						writer.close();
+						frame.dispose();
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					}
+					
+					LoginScreen.main(args);
 				}
-				LoginScreen.main(args);
+				
 				
 			}
 			
 		});
 		
-		/**
-		 * Sets up the form window by adding the Jlabels, buttons and  Jtextfield created above
-		 * Specifies the orientation and spacing 
-		 */
+		
+		
 		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
 		groupLayout.setHorizontalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
+			groupLayout.createParallelGroup(Alignment.TRAILING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(207)
+					.addComponent(btnSubmit)
+					.addContainerGap(198, Short.MAX_VALUE))
 				.addGroup(groupLayout.createSequentialGroup()
 					.addContainerGap()
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(14)
 							.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
-								.addGroup(groupLayout.createSequentialGroup()
-									.addGap(14)
-									.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
-										.addComponent(lblDateOfBirth)
-										.addComponent(lblBirthDay)
-										.addComponent(lblBirthMonth)
-										.addComponent(lblSchoolsName)
-										.addComponent(lblEmailAddress))
-									.addPreferredGap(ComponentPlacement.UNRELATED))
-								.addGroup(groupLayout.createSequentialGroup()
-									.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-										.addComponent(lblLastName, GroupLayout.DEFAULT_SIZE, 102, Short.MAX_VALUE)
-										.addComponent(lblFirstName, Alignment.TRAILING))
-									.addPreferredGap(ComponentPlacement.UNRELATED))
-								.addGroup(groupLayout.createSequentialGroup()
-									.addGap(37)
-									.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
-										.addComponent(lblPassword)
-										.addComponent(lblNewLabel))
-									.addGap(12)))
-							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
-								.addComponent(emailID)
-								.addComponent(studentID, GroupLayout.DEFAULT_SIZE, 287, Short.MAX_VALUE)
-								.addComponent(schoolName)
-								.addComponent(birthYear)
-								.addComponent(lastName, 282, 282, Short.MAX_VALUE)
-								.addComponent(firstName, 282, 282, Short.MAX_VALUE)
-								.addComponent(birthMonth)
-								.addComponent(birthDay)
-								.addComponent(passwordField))
-							.addGap(95))
+								.addComponent(lblSchoolsName)
+								.addComponent(lblEmailAddress)
+								.addComponent(lblNewLabel)
+								.addComponent(lblPassword)))
+						.addComponent(lblLastName, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 105, Short.MAX_VALUE)
+						.addComponent(lblFirstName)
+						.addComponent(lblAccountType)
+						.addComponent(lblDateOfBirthmdy))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(emailID, 287, 287, Short.MAX_VALUE)
+						.addComponent(studentID, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 287, Short.MAX_VALUE)
+						.addComponent(passwordField, 287, 287, Short.MAX_VALUE)
+						.addComponent(role, 0, 287, Short.MAX_VALUE)
+						.addComponent(schoolName, 287, 287, Short.MAX_VALUE)
+						.addComponent(firstName, GroupLayout.DEFAULT_SIZE, 287, Short.MAX_VALUE)
 						.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
-							.addComponent(btnSubmit)
-							.addGap(214))))
+							.addComponent(month, 0, 121, Short.MAX_VALUE)
+							.addGap(18)
+							.addComponent(day, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE)
+							.addGap(18)
+							.addComponent(year, GroupLayout.PREFERRED_SIZE, 70, GroupLayout.PREFERRED_SIZE))
+						.addComponent(lastName, GroupLayout.DEFAULT_SIZE, 287, Short.MAX_VALUE))
+					.addGap(58))
 		);
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
@@ -249,44 +252,46 @@ public class Form {
 					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
 						.addComponent(firstName, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 						.addComponent(lblFirstName))
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lastName, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblLastName))
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(birthYear, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblDateOfBirth))
-					.addGap(18)
-					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(birthMonth, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblBirthMonth))
-					.addGap(18)
-					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblBirthDay)
-						.addComponent(birthDay, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
 					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(lblLastName)
+						.addComponent(lastName, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(18)
+							.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+								.addComponent(month, GroupLayout.PREFERRED_SIZE, 21, GroupLayout.PREFERRED_SIZE)
+								.addComponent(lblDateOfBirthmdy)))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(18)
+							.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+								.addComponent(year, GroupLayout.PREFERRED_SIZE, 21, GroupLayout.PREFERRED_SIZE)
+								.addComponent(day, GroupLayout.PREFERRED_SIZE, 21, GroupLayout.PREFERRED_SIZE))))
+					.addGap(49)
 					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
 						.addComponent(lblSchoolsName)
 						.addComponent(schoolName, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
 					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
 						.addComponent(emailID, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 						.addComponent(lblEmailAddress))
-					.addPreferredGap(ComponentPlacement.RELATED)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
 					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblNewLabel)
-						.addComponent(studentID, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addComponent(studentID, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblNewLabel))
 					.addPreferredGap(ComponentPlacement.UNRELATED)
 					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
 						.addComponent(passwordField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 						.addComponent(lblPassword))
 					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(role, GroupLayout.PREFERRED_SIZE, 22, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblAccountType))
+					.addPreferredGap(ComponentPlacement.RELATED, 36, Short.MAX_VALUE)
 					.addComponent(btnSubmit)
-					.addGap(32))
+					.addGap(19))
 		);
 		frame.getContentPane().setLayout(groupLayout);
 		
 	}
-	
 }
