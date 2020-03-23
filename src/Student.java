44327bb7242f59fb4277d02252f4b8a3271e7f9f
shamimkhalili <47364526+shamimkhalili@users.noreply.github.com>
2020-03-23 15:08:59 -0600

    Added comments

--- src/Student.java
+++ src/Student.java
@@ -1,5 +1,3 @@
-package scholarship;
-
 /**
  * This is the initial Student class created for iteration 2 
  * @author Shamim 
@@ -12,26 +10,50 @@ public class Student extends User {
 	private Scholarship[] notAccepted;
 	private int GPA;
 	
+	/**
+	 * Get the list of courses of the student. 
+	 * @return the courses
+	 */
 	public Course[] getCourses() {
 		return courses;
 	}
 
+	/**
+	 * Get the list of pending scholarships. 
+	 * @return the pending
+	 */
 	public Scholarship[] getPending() {
 		return pending;
 	}
 
+	/**
+	 * Get the list of awaiting scholarships. 
+	 * @return the awaiting
+	 */
 	public Scholarship[] getAwaiting() {
 		return awaiting;
 	}
 
+	/**
+	 * Get the list of not accepted scholarships. 
+	 * @return the notAccepted
+	 */
 	public Scholarship[] getNotAccepted() {
 		return notAccepted;
 	}
 
+	/**
+	 * Get the student's GPA. 
+	 * @return the GPA
+	 */
 	public int getGPA() {
 		return GPA;
 	}
 
+	/**
+	 * Checks if student has a grade above F or W. 
+	 * @return boolean value hasGrade
+	 */
 	public boolean hasGrade(String grade) {
 		if(grade == "W" | grade == "F") {
 			return false;
