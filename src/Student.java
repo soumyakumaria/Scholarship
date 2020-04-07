cc415ee56315feaaed3a14ea6d284c7b473f3942
CaHerm <cam2000h@gmail.com>
2020-04-07 15:47:39 -0600

    Removed courses, added methods for adding Scholarships
    Also added grades and GPA calculation

--- src/Student.java
+++ src/Student.java
@@ -1,21 +1,47 @@
+package users;
+
+import scholarship.Scholarship;
+
 /**
  * This is the initial Student class created for iteration 2 
- * @author Shamim 
+ * @author Shamim, cam 
  *
  */
 public class Student extends User {
-	private Course[] courses;
+	private String grades;
 	private Scholarship[] pending;
 	private Scholarship[] awaiting;
 	private Scholarship[] notAccepted;
-	private int GPA;
+	
 	
 	/**
-	 * Get the list of courses of the student. 
-	 * @return the courses
+	 * sets all Scholarship arrays to a length of 0
 	 */
-	public Course[] getCourses() {
-		return courses;
+	public void initializeScholarships() {
+		pending = new Scholarship[0];
+		awaiting = new Scholarship[0];
+		notAccepted = new Scholarship[0];
+	}
+	
+	/**
+	 * @param s add this Scholarship to pending
+	 */
+	public void addPending(Scholarship s) {
+		pending = addScholarship(pending, s);
+	}
+	
+	/**
+	 * @param s add this Scholarship to awaiting
+	 */
+	public void addAwaiting(Scholarship s) {
+		awaiting = addScholarship(awaiting, s);
+	}
+	
+	/**
+	 * @param s add this Scholarship to notAccepted
+	 */
+	public void addNotAccepted(Scholarship s) {
+		notAccepted = addScholarship(notAccepted, s);
 	}
 
 	/**
@@ -41,26 +67,65 @@ public class Student extends User {
 	public Scholarship[] getNotAccepted() {
 		return notAccepted;
 	}
+	
+	public void setPending(Scholarship[] pending) {
+		this.pending = pending;
+	}
+
+	public void setAwaiting(Scholarship[] awaiting) {
+		this.awaiting = awaiting;
+	}
+
+	public void setNotAccepted(Scholarship[] notAccepted) {
+		this.notAccepted = notAccepted;
+	}
+
+	/**
+	 * @return grades, formatted as: "A,A+,F,B-,..."
+	 */
+	public String getGrades() {
+		return grades;
+	}
+
+	/**
+	 * @param grades must follow format: "A,A+,F,W,A,..."
+	 */ 
+	public void setGrades(String grades) {
+		this.grades = grades;
+	}
 
 	/**
 	 * Get the student's GPA. 
 	 * @return the GPA
 	 */
-	public int getGPA() {
-		return GPA;
+	public double getGPA() {
+		String[] letters = new String[] {"F", "D", "D+", "C-", "C", "C+", "B-", "B", "B+", "A-", "A", "A+"};
+		double[] numbers = new double[] {0, 1, 1.3, 1.7, 2, 2.3, 2.7, 3, 3.3, 3.7, 4, 4};
+		String[] gradesArr = grades.split(",");
+		double cumulativeGPA = 0;
+		int wCount = 0;
+		for (String s : gradesArr) {
+			if( s.equals("W")) {
+				wCount++;
+			} else {
+				for (int i = 0; i < letters.length; i++) {
+					if (letters[i].equals(s)) {
+						cumulativeGPA += numbers[i];
+						break;
+					}
+				}
+			}
+			
+		}
+		return cumulativeGPA/(gradesArr.length - wCount);
 	}
 
 	/**
-	 * Checks if student has a grade above F or W. 
+	 * Checks if student has a given grade. 
 	 * @return boolean value hasGrade
 	 */
 	public boolean hasGrade(String grade) {
-		if(grade == "W" | grade == "F") {
-			return false;
-		}
-		else{
-			return true;
-		}
+		return grades.contains(grade);
 	}
 
 
