ec623b3c2a985fff1209dd214e9c900d9672d42d
Tiffanyt1201 <46972701+Tiffanyt1201@users.noreply.github.com>
2020-04-10 16:35:14 -0600

    Update StudentWindow.java

--- src/Login/StudentWindow.java
+++ src/Login/StudentWindow.java
@@ -1,6 +1,7 @@
 package Login;
 
 import java.awt.EventQueue;
+import java.awt.Font;
 
 import javax.swing.JFrame;
 import javax.swing.JButton;
@@ -8,11 +9,12 @@ import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
 public class StudentWindow {
 	/**
 	 * 
-	 * @author Shamim Khalili
+	 * @author Shamim Khalili, Tiffany Tang
 	 * This class is the window for when a student
 	 * successfully logs in
 	 *
@@ -47,35 +49,67 @@ public class StudentWindow {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
+		frame = new JFrame("Student Homepage");
 		frame.setBounds(100, 100, 553, 434);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Emerging");
+		lblNewLabel.setFont(new Font("Sitka Text", Font.BOLD, 25));
+		lblNewLabel.setBounds(21, 10, 131, 26);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("University");
+		lblNewLabel_1.setFont(new Font("Sitka Text", Font.BOLD, 25));
+		lblNewLabel_1.setBounds(21, 36, 131, 26);
+		frame.getContentPane().add(lblNewLabel_1);
+		
 		/**
 		 * This button takes the user out of this window
 		 * back to the initial log in screen
 		 */
 		JButton btnNewButton = new JButton("Log out");
+		btnNewButton.setFont(new Font("Arial", Font.BOLD, 13));
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				frame.dispose();
-					LoginScreen.main(args);
+				frame.dispose(); 
+				JOptionPane.showMessageDialog(null, "You have successfully logged out.");
+				LoginScreen.main(args);
 
 			}
 		});
-		btnNewButton.setBounds(423, 11, 89, 23);
+		btnNewButton.setBounds(417, 36, 89, 23);
 		frame.getContentPane().add(btnNewButton);
 		
-		JLabel lblNewLabel = new JLabel("Student Name:");
-		lblNewLabel.setBounds(40, 15, 89, 14);
-		frame.getContentPane().add(lblNewLabel);
-		
-		JLabel lblNewLabel_1 = new JLabel("Student ID:");
-		lblNewLabel_1.setBounds(40, 40, 89, 14);
-		frame.getContentPane().add(lblNewLabel_1);
-		
 		JButton btnNewButton_1 = new JButton("View Scholarships");
-		btnNewButton_1.setBounds(40, 123, 185, 23);
+		btnNewButton_1.setFont(new Font("Arial", Font.BOLD, 13));
+		btnNewButton_1.setBounds(188, 191, 154, 29);
 		frame.getContentPane().add(btnNewButton_1);
+		
+		/**
+		 * This label will display the name of the user that logged in. This is from the name variable in 
+		 * the LoginScreen class. 
+		 */
+		JLabel lblNewLabel_2 = new JLabel(LoginScreen.name);
+		lblNewLabel_2.setBounds(123, 81, 92, 26);
+		frame.getContentPane().add(lblNewLabel_2);
+		
+		/**
+		 * This label will display the id of the user that logged in. This is from the id variable in 
+		 * the LoginScreen class. 
+		 */
+		JLabel lblNewLabel_3 = new JLabel(LoginScreen.id);
+		lblNewLabel_3.setBounds(123, 111, 92, 26);
+		frame.getContentPane().add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_4 = new JLabel("Student Name:");
+		lblNewLabel_4.setFont(new Font("Arial", Font.BOLD, 13));
+		lblNewLabel_4.setBounds(21, 81, 94, 26);
+		frame.getContentPane().add(lblNewLabel_4);
+		
+		JLabel lblNewLabel_5 = new JLabel("Student ID:");
+		lblNewLabel_5.setFont(new Font("Arial", Font.BOLD, 13));
+		lblNewLabel_5.setBounds(45, 111, 70, 26);
+		frame.getContentPane().add(lblNewLabel_5);
 	}
 }
