422492046135dc8e686872e04f494e24f00419ec
soumyakumaria <47364607+soumyakumaria@users.noreply.github.com>
2020-04-11 18:53:41 -0600

    updated during and after discord call

--- src/scholarship/ViewScholarship.java
+++ src/scholarship/ViewScholarship.java
@@ -77,8 +77,10 @@ public class ViewScholarship {
 		JScrollPane scrollPane = new JScrollPane();
 		
 		JLabel lblAllScholarships = new JLabel("All Scholarships");
-		JLabel lblScolarshipsYouCan = new JLabel("Scholarships you can apply for according to your GPA");
-		frame.setBounds(100, 100, 538, 623);
+		JLabel lblScolarshipsGPA = new JLabel("Scholarships you can apply for according to your GPA");
+		JLabel lblScholarshipYouCan = new JLabel("Scholarship you can apply for based on your GPA and single letter grade");
+		
+		frame.setBounds(100, 100, 559, 685);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		
@@ -91,9 +93,10 @@ public class ViewScholarship {
 		double GPA = 0.0;
 		Student s = Login.LoginScreen.s;
 		GPA = s.getGPA();
-		String grades = "A,B,C,A";
-		s.setGrades(grades);
 		
+		
+		
+		//Getting the sorted array of scholarships based on the student's GPA
 		Scholarship[] b  = Login.Utilities.sortGPA(a, GPA);
 		String sortArray [] = new String [b.length];
 		for(int i=0; i<b.length;i ++){
@@ -101,12 +104,15 @@ public class ViewScholarship {
 		}
 		
 		
-		
+		//Getting the sorted array of scholarships based on the student's GPA and single letter grade.
+		//An array of all scholarships and a student object is passed to the sort legible method.
 		Scholarship[] c  = Login.Utilities.sortLegible(a, s);
 		String sortLegibleArray [] = new String [c.length];
 		for(int i=0; i<c.length;i ++){
 			sortLegibleArray[i]= c[i].getName();	
 		}
+		
+		
 		
 		
 		
@@ -116,11 +122,100 @@ public class ViewScholarship {
 		JComboBox scholarshipName = new JComboBox(strArray);
 		JComboBox gpaScholarshipName = new JComboBox(sortArray);
 		JComboBox sortLegibleComboBox = new JComboBox(sortLegibleArray);
+		/**
+		 * This action handler finds the all the information about the scholarship selected 
+		 * by the user and displays it in the text area. The scholarship available in the combo box
+		 * is dependent on the GPA of the student. 
+		 */
+		gpaScholarshipName.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String temp = "";
+				temp = (String) gpaScholarshipName.getSelectedItem();
+				File scholarships = new File("scholarships.txt");
+				System.out.println(temp);
+				String info ="";
+				
+				
+				
+				for(int i = 0; i <1; i++) {
+					Scanner read = null;
+					
+					try {
+						read = new Scanner(scholarships);
+						while(read.hasNextLine()) {
+							if(read.nextLine().equals(temp)) {
+								for(int j=0; j<9;j++) {
+									info = info+read.nextLine()+"\n";
+								 
+								}
+								textArea.setText(info);
+								
+							}
+							
+						
+						}
+					} catch (FileNotFoundException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					
+					read.close();
+					
+				}
+				
+			}
+		});
+		/**
+		 * This action handler finds the all the information about the scholarship selected 
+		 * by the user and displays it in the text area. The scholarship available in the combo box
+		 * is dependent on the GPA and a single letter grade of the student. 
+		 */
+		
+		sortLegibleComboBox.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String temp = "";
+				temp = (String) sortLegibleComboBox.getSelectedItem();
+				File scholarships = new File("scholarships.txt");
+				System.out.println(temp);
+				String info ="";
+				
+				
+				
+				for(int i = 0; i <1; i++) {
+					Scanner read = null;
+					
+					try {
+						read = new Scanner(scholarships);
+						while(read.hasNextLine()) {
+							if(read.nextLine().equals(temp)) {
+								for(int j=0; j<9;j++) {
+									info = info+read.nextLine()+"\n";
+								 
+								}
+								textArea.setText(info);
+								
+							}
+							
+						
+						}
+					} catch (FileNotFoundException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					
+					read.close();
+					
+				}
+				
+			
+				
+			}
+		});
 		
 		/**
-		 *This action handler displays 
-		 *the information for each scholarship selected in the
-		 *JscrollPane.  
+		 * This action handler finds the all the information about the scholarship selected 
+		 * by the user and displays it in the text area. The scholarship available in the combo box
+		 * consists of all the scholarships.
 		 */
 		scholarshipName.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -166,73 +261,55 @@ public class ViewScholarship {
 		
 		scholarshipName.setToolTipText("Select");
 		
-		
-		
-		JLabel lblScholarshipYouCan = new JLabel("Scholarship you can apply for based on your GPA and single letter grade");
-		
-		
-		
-		
-		
-		
-	
-		
-		
-		
-		
-		
-		
-		
-		
 		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
 		groupLayout.setHorizontalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(229)
+					.addGap(227)
 					.addComponent(btnApply)
-					.addGap(148))
+					.addContainerGap(251, Short.MAX_VALUE))
 				.addGroup(groupLayout.createSequentialGroup()
 					.addGap(53)
 					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
 						.addGroup(groupLayout.createSequentialGroup()
-							.addComponent(lblScholarshipYouCan)
+							.addComponent(lblAllScholarships)
 							.addContainerGap())
 						.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
 							.addGroup(groupLayout.createSequentialGroup()
-								.addComponent(lblScolarshipsYouCan)
+								.addComponent(lblScholarshipYouCan)
 								.addContainerGap())
 							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
 								.addGroup(groupLayout.createSequentialGroup()
-									.addComponent(lblAllScholarships)
+									.addComponent(lblScolarshipsGPA)
 									.addContainerGap())
 								.addGroup(groupLayout.createSequentialGroup()
 									.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
-										.addComponent(scholarshipName, 0, 412, Short.MAX_VALUE)
-										.addComponent(gpaScholarshipName, Alignment.LEADING, 0, 412, Short.MAX_VALUE)
-										.addComponent(sortLegibleComboBox, Alignment.LEADING, 0, 412, Short.MAX_VALUE)
-										.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 412, Short.MAX_VALUE))
+										.addComponent(gpaScholarshipName, Alignment.LEADING, 0, 433, Short.MAX_VALUE)
+										.addComponent(scrollPane, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 433, Short.MAX_VALUE)
+										.addComponent(sortLegibleComboBox, Alignment.LEADING, 0, 433, Short.MAX_VALUE)
+										.addComponent(scholarshipName, Alignment.LEADING, 0, 433, Short.MAX_VALUE))
 									.addGap(55))))))
 		);
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(lblScolarshipsYouCan)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(gpaScholarshipName, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addGap(24)
-					.addComponent(lblScholarshipYouCan)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(sortLegibleComboBox, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addGap(42)
+					.addComponent(lblScolarshipsGPA)
 					.addGap(18)
+					.addComponent(gpaScholarshipName, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addGap(37)
+					.addComponent(lblScholarshipYouCan)
+					.addGap(18)
+					.addComponent(sortLegibleComboBox, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addGap(38)
 					.addComponent(lblAllScholarships)
 					.addPreferredGap(ComponentPlacement.RELATED)
 					.addComponent(scholarshipName, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addGap(36)
+					.addGap(57)
 					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 206, GroupLayout.PREFERRED_SIZE)
-					.addGap(34)
+					.addPreferredGap(ComponentPlacement.RELATED, 35, Short.MAX_VALUE)
 					.addComponent(btnApply, GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(85, Short.MAX_VALUE))
+					.addGap(41))
 		);
 		
 		scrollPane.setViewportView(textArea);
@@ -243,43 +320,5 @@ public class ViewScholarship {
 	
 
 
-	/**
-	 * @return a list of all Scholarship objects stored in scholarships.txt.
-	 */
-	public static Scholarship[] loadScholarships() {
-		try {
-			File file = new File("scholarships.txt");
-			Scanner in = new Scanner(file);
-			int n = 0;
-			while(in.hasNextLine()) {
-				n++;
-				in.nextLine();
-			}
-			in = new Scanner(file);
-			Scholarship[] a = new Scholarship[n / 10];
-			
-			int i = 0;
-			while (i < (n / 10)) {
-				Scholarship s = new Scholarship();
-				s.setName(in.nextLine());
-				s.setMinGPA(in.nextLine());
-				s.setAntirequisite(in.nextLine());
-				s.setNumAvailable(Integer.parseInt(in.nextLine()));
-				s.setNominations(in.nextLine().split("/"));
-				s.setGrant(in.nextLine().equals("true"));
-				s.setDescription(in.nextLine());
-				s.setMoney(Double.parseDouble(in.nextLine()));
-				s.setFrequency(in.nextLine());
-				s.setDuration(in.nextLine());
-				a[i] = s;
-				i++;
-			}
-			
-			in.close();
-			return a;
-		} catch(FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
+	
 }
